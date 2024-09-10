eccab096ec33d613ec9944a82581002bd76e1ce4
@dev.arthur <contato.arthurreis222@gmail.com>
2024-09-10 15:54:15 -0300

    chore: setup tailwind and plugins

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            colors: {
+                dark: {
+                    10: '#121214',
+                    20: '#F3F7FF',
+                    30: '#FFFFFF',
+                },
+                'brand-color': {
+                    10: '#7CA2F4',
+                },
+            },
+        },
+    },
+    plugins: [],
+};
