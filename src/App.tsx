cd562e86c782ffb0d49000cd5152da0d26970607
@dev.arthur <contato.arthurreis222@gmail.com>
2024-09-10 16:00:14 -0300

    feat: add Header Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <>
+      <Header />
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  return (
+    <header className="flex h-10 w-full items-center justify-center bg-dark-20 font-medium text-sm">
+      Frete grátis para todo o Brasil
+    </header>
+  );
+};
+
+export default Header;
+// 12px, 802px, 12px, 802px
